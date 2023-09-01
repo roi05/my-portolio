bb74ead7041fa5441d850f51808f19d076e0f9e5
Roi Buenaventura <roibuenaventura05@gmail.com>
2023-09-01 11:24:53 +0800

    edit_header_and_change_social_media_project

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className='h-20 mb-28 text-lg flex justify-center sm:justify-end gap-x-14 flex-wrap items-center '>
+    <header className='h-20 mb-28 text-lg flex justify-center sm:justify-end gap-x-14 flex-wrap items-center border-b'>
       <div className='cursor-pointer'>Home</div>
       <div className='cursor-pointer'>
         <a href='#about-me'>About Me</a>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import budgetTracker from '@/public/expense-tracker-app.png';
-import socialMedia from '@/public/blog-post.png';
+import socialMedia from '@/public/socialmedia_app.png';
 import ecommerce from '@/public/e-commerce.png';
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
@@ -59,8 +59,49 @@ export default function Projects() {
       <div>
         <div className='grid grid-cols-1  sm:grid-cols-2 gap-5 p-5'>
           <div
-            className='w-10/12 mx-auto min-h-64 max-h-72 overflow-hidden rounded-xl'
+            className='w-10/12 mx-auto min-h-64 max-h-72 overflow-hidden '
             ref={project1Ref}>
+            <a
+              href='https://socialsphere-three.vercel.app'
+              target='_blank'>
+              <Image
+                src={socialMedia}
+                alt='Social Media App'
+                placeholder='blur'
+                className='rounded-xl'
+              />
+            </a>
+          </div>
+          <div>
+            <motion.h1
+              className='text-6xl font-thin text-blue-100'
+              initial='hidden'
+              animate={project1InView ? 'visible' : 'hidden'}
+              variants={projectVariants}>
+              Project 1
+            </motion.h1>
+            <h2 className='text-xl pl-3 my-4'>Social Media App</h2>
+            <motion.p
+              className='pl-3 text-lg font-light tracking-wider'
+              initial='hidden'
+              animate={project1InView ? 'visible' : 'hidden'}
+              variants={projectVariants}>
+              A friendly social media app that lets you connect with others in a
+              whole new way. Share posts, leave comments, follow interesting
+              people, and give likes to what you enjoy. You'll never miss a beat
+              with notifications to keep you in the loop. Powered by modern
+              technology like React, Next.js, and Tailwind CSS for the looks,
+              and using Prisma and MySQL for the behind-the-scenes stuff. Rest
+              easy knowing that the authentication keeps your journey through
+              the app safe and sound.
+            </motion.p>
+          </div>
+        </div>
+
+        <div className='grid grid-cols-1  sm:grid-cols-2 gap-5 p-5'>
+          <div
+            className='w-10/12 mx-auto min-h-64 max-h-72 overflow-hidden rounded-xl'
+            ref={project2Ref}>
             <a
               target='_blank'
               href='https://expense-tracker-app-aats.onrender.com'>
@@ -75,15 +116,15 @@ export default function Projects() {
             <motion.h1
               className='text-6xl font-thin text-blue-100'
               initial='hidden'
-              animate={project1InView ? 'visible' : 'hidden'}
+              animate={project2InView ? 'visible' : 'hidden'}
               variants={projectVariants}>
-              Project 1
+              Project 2
             </motion.h1>
             <h2 className='text-xl pl-3 my-4'>Budget tracker app</h2>
             <motion.p
               className='pl-3 text-lg font-light tracking-wider'
               initial='hidden'
-              animate={project1InView ? 'visible' : 'hidden'}
+              animate={project2InView ? 'visible' : 'hidden'}
               variants={projectVariants}>
               I've developed a user-friendly app that helps you track your daily
               expenses. It's built using React and MUI for a modern design. The
@@ -101,7 +142,7 @@ export default function Projects() {
         <div className='grid grid-cols-1  sm:grid-cols-2 gap-5 p-5'>
           <div
             className='w-10/12 mx-auto min-h-64 max-h-72 overflow-hidden rounded-xl'
-            ref={project2Ref}>
+            ref={project3Ref}>
             <a
               href='https://my-frist-e-commerce.vercel.app'
               target='_blank'>
@@ -116,15 +157,15 @@ export default function Projects() {
             <motion.h1
               className='text-6xl font-thin text-blue-100'
               initial='hidden'
-              animate={project2InView ? 'visible' : 'hidden'}
+              animate={project3InView ? 'visible' : 'hidden'}
               variants={projectVariants}>
-              Project 2
+              Project 3
             </motion.h1>
             <h2 className='text-xl pl-3 my-4'>E commerce</h2>
             <motion.p
               className='pl-3 text-lg font-light tracking-wider'
               initial='hidden'
-              animate={project2InView ? 'visible' : 'hidden'}
+              animate={project3InView ? 'visible' : 'hidden'}
               variants={projectVariants}>
               In this example, I have utilized the latest update of Next.js, a
               popular framework for building React applications. To enhance type
@@ -137,46 +178,6 @@ export default function Projects() {
               access of data between components. This combination of
               technologies ensures a robust and well-designed application with
               enhanced type safety and seamless data management.
-            </motion.p>
-          </div>
-        </div>
-
-        <div className='grid grid-cols-1  sm:grid-cols-2 gap-5 p-5'>
-          <div
-            className='w-10/12 mx-auto min-h-64 max-h-72 overflow-hidden rounded-xl'
-            ref={project3Ref}>
-            <a
-              href='https://makepost.vercel.app'
-              target='_blank'>
-              <Image
-                src={socialMedia}
-                alt='Social Media App'
-                placeholder='blur'
-              />
-            </a>
-          </div>
-          <div>
-            <motion.h1
-              className='text-6xl font-thin text-blue-100'
-              initial='hidden'
-              animate={project3InView ? 'visible' : 'hidden'}
-              variants={projectVariants}>
-              Project 3
-            </motion.h1>
-            <h2 className='text-xl pl-3 my-4'>Social Media App</h2>
-            <motion.p
-              className='pl-3 text-lg font-light tracking-wider'
-              initial='hidden'
-              animate={project3InView ? 'visible' : 'hidden'}
-              variants={projectVariants}>
-              The app allows posting, liking, and exploring content. It
-              leverages TANStack for seamless CRUD operations with optimistic
-              updates. TypeScript ensures reliable development, while Tailwind
-              CSS enhances the app's design. Authentication and authorization
-              are handled securely with NextAuth. On the backend, Prisma with
-              PostgreSQL provides efficient data storage. Enjoy a seamless
-              experience with modern technologies, sleek design, and robust
-              security.
             </motion.p>
           </div>
         </div>
